b98b751dc86d49571c09b4b7d152c745267807c9
Gabriel Yashim <gabrileyashim405@gmail.com>
2022-04-08 11:27:54 +0100

    update requirement.txt

--- app.py
+++ app.py
@@ -9,19 +9,16 @@ from __future__ import division, print_function
 import sys
 import os
 import glob
-import re
 import numpy as np
 import cv2
 
 # Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from keras.models import load_model
 from keras.preprocessing import image
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
